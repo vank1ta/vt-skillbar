945f409d45b2ea6cad279091da60b487ab793f2d
vank1ta <banowski0810@gmail.com>
2022-05-08 17:17:19 +0200

    new resoucer
    Edit all resourcer

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,4 +1,10 @@
--- Author - FuryX#6060
+fx_version 'cerulean'
+games      { 'gta5' }
+lua54 'yes'
+
+author 'Vank1ta Scripts'
+description 'NoPixel 3.0 SkillBar'
+version '1.1.0'
 
 ui_page 'index.html'
 
