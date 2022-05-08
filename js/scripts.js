c9878551927c969d2fcfe63d5da667e9d54468f1
vank1ta <banowski0810@gmail.com>
2022-05-08 17:12:29 +0200

    scripts.js
    I changed the name of the resource and added the check for the new cfx / fivem user protection standard for links integrated in the js request.

--- js/scripts.js
+++ js/scripts.js
@@ -74,7 +74,7 @@ $(document).ready(function() {
       if (degrees >= new_degrees) {
         if (streak > 0) {
             closeMain();
-            $.post('http://vank1ta-skillbar/taskCancel', JSON.stringify({
+            $.post('https://vt-skillbar/taskCancel', JSON.stringify({
                 tasknum: curTask
             }));
             streak = 0;
@@ -92,7 +92,7 @@ $(document).ready(function() {
       streak++;
       if (streak > 2) {
           closeMain();
-          $.post('http://vank1ta-skillbar/taskEnd', JSON.stringify({
+          $.post('https://vt-skillbar/taskEnd', JSON.stringify({
               taskResult: percent
           }));
       } else {
@@ -115,7 +115,7 @@ $(document).ready(function() {
 
       if (item.closeFail === true) {
           closeMain();
-          $.post('http://vank1ta-skillbar/taskCancel', JSON.stringify({
+          $.post('https://vt-skillbar/taskCancel', JSON.stringify({
               tasknum: curTask
           }));
           streak = 0;
@@ -142,14 +142,14 @@ $(document).ready(function() {
             if( degrees < d_start ){
                 // console.log('Failed: Too soon!');
                 closeMain();
-                $.post('http://vank1ta-skillbar/taskCancel', JSON.stringify({
+                $.post('https://vt-skillbar/taskCancel', JSON.stringify({
                     tasknum: curTask
                 }));
                 wrong();
             }else if( degrees > d_end ){
                 // console.log('Failed: Too late!');
                 closeMain();
-                $.post('http://vank1ta-skillbar/taskCancel', JSON.stringify({
+                $.post('https://vt-skillbar/taskCancel', JSON.stringify({
                     tasknum: curTask
                 }));
                 wrong();
@@ -160,7 +160,7 @@ $(document).ready(function() {
         }else{
             // console.log('Failed: Pressed '+key_pressed+' instead of '+key_to_press);
             closeMain();
-            $.post('http://vank1ta-skillbar/taskCancel', JSON.stringify({
+            $.post('https://vt-skillbar/taskCancel', JSON.stringify({
                 tasknum: curTask
             }));
             wrong();
